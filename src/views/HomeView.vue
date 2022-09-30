49ed05649cdbe412a8f85176685ce608286510ef
Sam Sharff <samanthaleec@gmail.com>
2022-09-30 11:18:42 -0400

    adds video to Olerud home run (gm 2); adjusts so video opens in new tab

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -484,7 +484,12 @@ export default {
           <td style="border: 1px solid #ccc; padding: 2px 3px 2px 2px; white-space: nowrap">2.56%</td>
           <td style="border: 1px solid #ccc; padding: 2px 3px 2px 2px; white-space: nowrap">30.25</td>
           <td csk="23.046" style="border: 1px solid #ccc; padding: 2px 3px 2px 2px; white-space: nowrap">
-            Home Run (Line Drive); Posada Scores
+            <a
+              href="https://www.youtube.com/watch?v=DHSgINljo5A&feature=share&utm_source=EJGixIgBCJiu2KjB4oSJEQ"
+              target="_blank"
+            >
+              Home Run (Line Drive); Posada Scores
+            </a>
           </td>
         </tr>
         <tr>
